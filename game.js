16de1a358a42e98e65d47d01229862baa6f3370b
Kabir Virji <kabirvirji@gmail.com>
2016-08-07 22:48:39 -0700

    displaying score variable debugging

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
 var score = 0;
+document.querySelector("score").innerHTML = score;
 
-var optionOne = document.getElementById("correct-option");
-optionOne.onclick = function() {
-  score++;
-  console.log(score);
-};
+// var optionOne = document.getElementById("correct-option");
+// optionOne.onclick = function() {
+//   score++;
+//   console.log(score);
+// };
 
-var optionTwo = document.getElementById("wrong-option-1");
-optionTwo.onclick = function() {
-  console.log(score);
-};
+// var optionTwo = document.getElementById("wrong-option-1");
+// optionTwo.onclick = function() {
+//   console.log(score);
+// };
 
-var optionThree = document.getElementById("wrong-option-2");
-optionThree.onclick = function() {
-  console.log(score);
-};
+// var optionThree = document.getElementById("wrong-option-2");
+// optionThree.onclick = function() {
+//   console.log(score);
+// };
+
+// document.querySelector("score").innerHTML = score;
