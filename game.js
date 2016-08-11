6456d2616a8dfca6119ad4f556a48c496510a8c2
Kabir Virji <kabirvirji@gmail.com>
2016-08-10 21:19:20 -0700

    fixed score increase

--- game.js
+++ game.js
@@ -1,6 +1,26 @@
 var score = 0;
 document.getElementById("score").innerHTML = "Score: " + score;
 
+var questionOne_one = document.getElementById("truth-1");
+var questionOne_two = document.getElementById("truth-2");
+var questionOne_three = document.getElementById("lie");
+
+function Lie(parameter) {
+	if (score == 1) {
+		var flag_one = false;
+	} else {
+		var flag_one = true;
+	}
+	if (flag_one) {
+		score++;
+		document.getElementById("score").innerHTML = "Score: " + score;
+	}
+}
+
+questionOne_three.addEventListener("click", Lie);
+
+
+
 // var optionOne = document.getElementById("correct-option");
 // optionOne.onclick = function() {
 //   score++;
